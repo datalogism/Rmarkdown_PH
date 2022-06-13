e345eaaf69ccf37438d4690489b54aaa9d5a1e7c
c_ringwald <celian.ringwald@hotmail.fr>
2022-06-14 00:12:15 +0200

    some style adaptation

---
+++ requierements.r
@@ -0,0 +1,14 @@
+install.packages("revealjs")
+library(revealjs)
+install.packages("sf")
+library(sf)
+
+install.packages("leaflet")
+library(leaflet)
+
+install.packages("sysfonts")
+library(sysfonts)
+font_add_google("Crete Round")
+install.packages("htmltools")
+
+#<iframe src="https://datalogism.github.io/viz/chord_diagram_role/index.html" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen="" allow="autoplay" data-external="1"></iframe>
